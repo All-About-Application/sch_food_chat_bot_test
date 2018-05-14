701f77f25ba23abdedf1b24569f198cc2dda041f
alstn2468 <alstn2468_@naver.com>
2018-05-14 13:21:32 +0900

    2018-05-14

--- app/views.py
+++ app/views.py
@@ -30,7 +30,7 @@ select_button = '[*] 선택한 버튼 : {0}\n[*] {1}의\n[*] {0} 메뉴입니다
 # 데이터를 보기 좋게 출력하기 위한 문자열 처리 함수
 def char_replace(meal) :
 
-	for key, value in app.my_module.repdic.trans_dic.items() :
+	for key, value in my_module.repdic.trans_dic.items() :
 
 		meal = meal.replace(key, value)
 
@@ -69,7 +69,7 @@ def re_process(output) :
             'keyboard':
 			{
                 'type': 'buttons',
-                'buttons' : app.my_module.button.basic_button
+                'buttons' : my_module.button.basic_button
             }
         }
 	)
@@ -86,7 +86,7 @@ def food_sel_process() :
 			'keyboard' :
 			{
 				'type' : 'buttons',
-				'buttons' : app.my_module.button.food_sel_process_button
+				'buttons' : my_module.button.food_sel_process_button
 			}
 		}
 	)
@@ -96,7 +96,7 @@ def keyboard(request) :
 	return JsonResponse (
 		{
 		'type' : 'buttons',
-		'buttons' : app.my_module.button.basic_button
+		'buttons' : my_module.button.basic_button
 		}
 	)
 
